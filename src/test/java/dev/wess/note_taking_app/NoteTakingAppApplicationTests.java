299ddf0854197277265d5d54753532bbb938dffb
Wessel van Beek <wessel.vanbeek@student.hu.nl>
2024-10-16 10:47:00 +0200

    initial commit from intelliJ

---
+++ src/test/java/dev/wess/note_taking_app/NoteTakingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wess.note_taking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteTakingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
